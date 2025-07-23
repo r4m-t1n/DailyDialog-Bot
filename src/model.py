6a5c5f3e497f3f14752d0910da0fb6038115db2f
r4m-t1n <im.r4mt1n@gmail.com>
2025-07-23 23:44:27 +0330

    add Decoder and seq2seq model

--- src/model.py
+++ src/model.py
@@ -1,21 +1,6 @@
 import torch
 import torch.nn as nn
 
-class LSTMClassifier(nn.Module):
-    def __init__(self, vocab_size, embedding_dim=100, hidden_dim=128, output_dim=1):
-        super().__init__()
-        self.embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx=0)
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim, batch_first=True)
-        self.fc = nn.Linear(hidden_dim, output_dim)
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-        embedded = self.embedding(x)
-        _, (hidden, _) = self.lstm(embedded)
-        hidden = hidden[-1]
-        out = self.fc(hidden)
-        return self.sigmoid(out)
-
 class Encoder(nn.Module):
     def __init__(self, vocab_size, embed_size, hidden_size, num_layers=1):
         super(Encoder, self).__init__()
@@ -26,3 +11,48 @@ class Encoder(nn.Module):
         embedded = self.embedding(x)
         _, (hidden, cell) = self.lstm(embedded)
         return hidden, cell
+
+class Decoder(nn.Module):
+    def __init__(self, vocab_size, embed_size, hidden_size, num_layers=1):
+        super(Decoder, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, embed_size, padding_idx=0)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, vocab_size)
+
+    def forward(self, x, hidden, cell):
+        x = x.unsqueeze(1)
+        embedded = self.embedding(x)
+        outputs, (hidden, cell) = self.lstm(embedded, (hidden, cell))
+        predictions = self.fc(outputs.squeeze(1))
+        return predictions, hidden, cell
+
+
+class Seq2Seq(nn.Module):
+    def __init__(self, encoder, decoder, device):
+        super(Seq2Seq, self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.device = device
+
+    def forward(self, source, target, teacher_forcing_ratio=0.5):
+        encoder_hidden, encoder_cell = self.encoder(source)
+
+        target_len = target.shape[1]
+        batch_size = target.shape[0]
+        vocab_size = self.decoder.fc.out_features
+
+        outputs = torch.zeros(target_len, batch_size, vocab_size).to(self.device)
+
+        decoder_input = target[:, 0]
+
+        hidden = encoder_hidden
+        cell = encoder_cell
+
+        for t in range(1, target_len):
+            prediction, hidden, cell = self.decoder(decoder_input, hidden, cell)
+            outputs[t] = prediction
+            teacher_force = torch.rand(1).item() < teacher_forcing_ratio
+            top1 = prediction.argmax(1)
+            decoder_input = target[:, t] if teacher_force else top1
+
+        return outputs
